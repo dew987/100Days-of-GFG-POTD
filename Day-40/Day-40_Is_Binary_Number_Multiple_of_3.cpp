c97307021eba869d9c0c73002972fae07a4e8d5c
Dewanshu Satpute <79638371+dew987@users.noreply.github.com>
2023-07-01 19:26:25 +0530

    Committing Day-40 Is Binary Number Multiple of 3 problem code

---
+++ Day-40/Day-40_Is_Binary_Number_Multiple_of_3.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+		
+	int isDivisible(string s){
+	    //complete the function here
+	    int ans= 0;
+	    for(int i= 0; i<s.length(); i++)
+	    {
+	       ans= (ans*2 + (s[i]- '0'))%3;
+	    }
+	 
+	    return ans== 0;
+	}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+    
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.isDivisible(s) << endl;
+    }
+return 0;
+}
+
+
+// } Driver Code Ends

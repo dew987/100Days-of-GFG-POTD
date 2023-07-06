dbeae5483a1ca7b76867e83fe710f2e95cd4d905
Dewanshu Satpute <79638371+dew987@users.noreply.github.com>
2023-07-06 11:16:47 +0530

    Committing Day-45 Buy and Sell Stocks II problem code

---
+++ Day-45/Day-45_Buy_and_Sell_Stocks_II.cpp
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    int stockBuyAndSell(int n, vector<int> &arr) {
+        // code here
+        int sum=0;
+        int low=0,high=0;
+        for(int i=0; i<n; i++)
+        {
+            if(arr[high]< arr[i])
+                high++;
+            else
+            {
+                sum+= arr[high]-arr[low];
+                low=i;
+                high=i;
+            }
+        }
+        sum+= arr[high]-arr[low];
+        return sum;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        vector<int> prices(n);
+        Array::input(prices,n);
+        
+        Solution obj;
+        int res = obj.stockBuyAndSell(n, prices);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends

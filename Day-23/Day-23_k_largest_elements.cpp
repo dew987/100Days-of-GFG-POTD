f5b3f70d910219b2351cba1d0ab1720c090270b1
Dewanshu Satpute <dewanshusatpude987@gmail.com>
2023-07-14 12:03:53 +0530

    Committing Day-23 K largest element Problem code

---
+++ Day-23/Day-23_k_largest_elements.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+//User function template for C++
+class Solution{
+public:	
+	vector<int> kLargest(int arr[], int n, int k) {
+	    // code here
+	    priority_queue<int> q1;
+	    vector<int> ans;
+	    for(int i=0; i<n; i++)
+	    {
+	        q1.push(arr[i]);
+	    }
+	    for(int i=0; i<k; i++)
+	    {
+	        ans.push_back(q1.top());
+	        q1.pop();
+	    }
+	    return ans;
+	}
+
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.kLargest(arr, n, k);
+        for (auto x : ans) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends

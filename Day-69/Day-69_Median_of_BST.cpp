2feb72217068fbb453ea99b1b522a023de8ec5de
Dewanshu Satpute <dewanshusatpude987@gmail.com>
2023-07-30 23:38:12 +0530

    Committing Day-69 Median of BST Problem code

---
+++ Day-69/Day-69_Median_of_BST.cpp
@@ -0,0 +1,165 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+
+float findMedian(struct Node* node);
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+   // Corner Case
+   if(str.length() == 0 || str[0] == 'N')
+       return NULL;
+
+   // Creating vector of strings from input
+   // string after spliting by space
+   vector<string> ip;
+
+   istringstream iss(str);
+   for(string str; iss >> str; )
+       ip.push_back(str);
+
+   // Create the root of the tree
+   Node* root = new Node(stoi(ip[0]));
+ 
+   // Push the root to the queue
+   queue<Node*> queue;
+   queue.push(root);
+
+   // Starting from the second element
+   int i = 1;
+   while(!queue.empty() && i < ip.size()) {
+
+       // Get and remove the front of the queue
+       Node* currNode = queue.front();
+       queue.pop();
+
+       // Get the current node's value from the string
+       string currVal = ip[i];
+
+       // If the left child is not null
+       if(currVal != "N") {
+
+           // Create the left child for the current node
+           currNode->left = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->left);
+       }
+
+       // For the right child
+       i++;
+       if(i >= ip.size())
+           break;
+       currVal = ip[i];
+
+       // If the right child is not null
+       if(currVal != "N") {
+
+           // Create the right child for the current node
+           currNode->right = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->right);
+       }
+       i++;
+   }
+
+   return root;
+}
+
+int main() {
+  
+   int t;
+   string tc;
+   getline(cin, tc);
+   t=stoi(tc);
+   //cout << t << endl;
+   while(t--)
+   {
+        string s; 
+       getline(cin, s);
+       Node* root = buildTree(s);
+
+      // getline(cin, s);
+       
+        cout << findMedian(root) << endl;
+
+      // cout<<"~"<<endl;
+   }
+   return 0;
+}
+// } Driver Code Ends
+
+
+/*
+Structure of the binary Search Tree is as
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+
+int countNodes(Node *root)
+{
+     if(root== NULL)
+      return 0;
+     
+     return 1 + countNodes(root->left) + countNodes(root->right);    
+}
+
+float solve(Node *root,int size,int &count)
+{
+    if(root==NULL)
+       return -1;
+    int flag=0;
+    float a=solve(root->left,size,count);
+    count++;
+    if((count==size/2&&(size%2==0))||count==size/2+1)
+    flag=1;
+    
+    float b=solve(root->right,size,count);
+    if(flag==1)
+    {
+        if(a!=-1)
+          return ((float)root->data+a)/2.0;
+        else if(b!=-1)
+          return ((float)root->data+b)/2.0;
+        else
+          return root->data;
+
+    }
+    return max(a,b);
+}
+float findMedian(struct Node *root)
+{
+      //Code here
+      int n=countNodes(root);
+      int count=0;
+      float ans=solve(root,n,count);
+      return ans;
+
+}
+
